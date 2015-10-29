430d2b25112461d7bce58559462cff881f017ccd
Devon Jones <devon.jones@sendgrid.com>
2015-10-28 19:40:12 -0600

    moving back to thermistor table 1

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -78,62 +78,8 @@
 #define EXTRUDER_0_AUTO_FAN_PIN   10
 #define EXTRUDER_1_AUTO_FAN_PIN   -1
 #define EXTRUDER_2_AUTO_FAN_PIN   -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]"}|
-[
-
-
-
-
-
-
-
-
-
-
-
-#[]]]+define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#defin][[e EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
-=]
-]
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
+#define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
 //===========================================================================
 //=============================Mechanical Settings===========================
 //===========================================================================
