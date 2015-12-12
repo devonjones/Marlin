e6dd540f9be5550769858d6ff9e50bea7f80b97f
Devon Jones <devon.jones@sendgrid.com>
2015-12-12 14:24:56 -0700

    lee's changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -324,7 +324,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
 #define X_HOME_DIR -1
-#define Y_HOME_DIR 1
+#define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
 #define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
