db99c4b72dd249f0b22a5dae740a264aec40cd98
Devon Jones <devon.jones@sendgrid.com>
2016-10-03 12:03:24 -0600

    adding more to morgan's config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -323,7 +323,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
-#define X_HOME_DIR -1
+#define X_HOME_DIR 1
 #define Y_HOME_DIR 1
 #define Z_HOME_DIR -1
 
