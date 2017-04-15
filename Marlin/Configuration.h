33c70ba208a97afed885b79ba33f4558069bed34
Devon Jones <devon.jones@sendgrid.com>
2017-04-15 11:24:13 -0600

    tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -476,7 +476,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {161,161,4000,1200}  // default steps per unit for Prusa i3
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {160,160,4000,1200}  // default steps per unit for Prusa i3
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {2000,2000,20,1000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
